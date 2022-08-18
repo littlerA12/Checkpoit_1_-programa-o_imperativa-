856f1a0e3025a2d08339b46e57866d346ecdc26c
littlerA12 <105258386+littlerA12@users.noreply.github.com>
2022-08-18 01:06:36 -0300

    Add files via upload

--- checkpoint/checkpoint.js
+++ checkpoint/checkpoint.js
@@ -5,7 +5,7 @@ let prato = [
     'feijao',
     'brigadeiro'
 ]
-let pedido=12
+let pedido=7
 console.log('Tempo(s):', pedido)
 let comida =[
     pipoca=10,
@@ -17,91 +17,133 @@ let comida =[
 
 switch(pedido){
     case(pedido):
-      if((pedido==comida[0]*3)){
+      if((pedido>=comida[0]*3)){
         console.log('KABUUUM!! -', prato[0])
+        console.log('Prato pronto,bom apetite')
     } 
-    else if((pedido==comida[0]*2)){
+    else if((pedido>=comida[0]*2)){
         console.log('A comida queimou... -', prato[0])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido<comida[0])){
         console.log('Tempo insuficiente -', prato[0])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido==comida[0])){
         console.log('Prato pronto,bom apetite -', prato[0])
+
     }
     else if((pedido<comida[0]*2)){
         console.log('Prato  inexistente -', prato[0])
+        console.log('Prato pronto,bom apetite')
+    }
+    else{
+        console.log('Prato inexistente -', prato[0])
+        console.log('Prato pronto,bom apetite')
     }
 
     case(pedido):
-      if((pedido==comida[1]*3)){
+      if((pedido>=comida[1]*3)){
         console.log('KABUUUM -', prato[1])
+        console.log('Prato pronto,bom apetite')
       } 
-    else if((pedido==comida[1]*2)) {
+    else if((pedido>=comida[1]*2)) {
         console.log('A comida queimou... -', prato[1])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido<comida[1])){
         console.log('Tempo insuficiente -', prato[1])
+        console.log('Prato pronto,bom apetite')
     }
     else if(pedido==comida[1]){
         console.log('Prato pronto,bom apetite -', prato[1])
     }
     else if((pedido<comida[1]*2)){
         console.log('Prato inexistente -', prato[1])
+        console.log('Prato pronto,bom apetite')
+    }
+    else{
+        console.log('Prato inexistente -', prato[1])
+        console.log('Prato pronto,bom apetite')
     }
     
 
     case(pedido):
-      if((pedido==comida[2]*3)){
+      if((pedido>=comida[2]*3)){
         console.log('KABUUUM -', prato[2])
+        console.log('Prato pronto,bom apetite')
       } 
-    else if((pedido==comida[2]*2)){
+    else if((pedido>=comida[2]*2)){
         console.log('A comida queimou... -', prato[2])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido<comida[2])) {
         console.log('Tempo insuficiente -', prato[2])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido==comida[2])){
         console.log('Prato pronto,bom apetite -', prato[2])
     }
     else if((pedido<comida[2]*2)){
         console.log('Prato inexistente -', prato[2])
+        console.log('Prato pronto,bom apetite')
+    }
+    else{
+        console.log('Prato inexistente -', prato[2])
+        console.log('Prato pronto,bom apetite')
     }
     
 
     case(pedido):
-      if(pedido==comida[3]*3){
+      if(pedido>=comida[3]*3){
         console.log('KABUUUM -', prato[3])
+        console.log('Prato pronto,bom apetite')
       } 
-    else if((pedido==comida[3]*2)){
+    else if((pedido>=comida[3]*2)){
         console.log('A comida queimou... -', prato[3])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido<comida[3])){
         console.log('Tempo insuficiente -', prato[3])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido==comida[3])){
         console.log('Prato pronto,bom apetite -', prato[3])
     }
     else if((pedido<comida[3]*2)){
         console.log('Prato inexistente -', prato[3])
+        console.log('Prato pronto,bom apetite')
+    }
+    else{
+        console.log('Prato inexistente -', prato[3])
+        console.log('Prato pronto,bom apetite')
     }
     
 
     case(pedido):
-      if((pedido==comida[4]*3)){
+      if((pedido>=comida[4]*3)){
         console.log('KABUUUM -', prato[4])
+        console.log('Prato pronto,bom apetite')
       } 
-    else if((pedido==comida[4]*2)){
+    else if((pedido>=comida[4]*2)){
         console.log('A comida queimou... -', prato[4])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido<comida[4])){
         console.log('Tempo insuficiente -', prato[4])
+        console.log('Prato pronto,bom apetite')
     }
     else if((pedido==comida[4])){
         console.log('Prato pronto,bom apetite -', prato[4])
     }
     else if((pedido<comida[4]*2)){
         console.log('Prato inexistente -', prato[4])
+        console.log('Prato pronto,bom apetite')
     }
+    else{
+        console.log('Prato inexistente -', prato[4])
+        console.log('Prato pronto,bom apetite')
+    }
+    
     break
 }
